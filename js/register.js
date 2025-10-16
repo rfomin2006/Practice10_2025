76ef9dc33235cfcf2328bcb1785b9b8ba3c2edc9
rfomin2006 <rfomin2805@gmail.com>
2025-10-16 14:10:56 +0700

    dev-initial

--- js/register.js
+++ js/register.js
@@ -4,37 +4,6 @@ const emailField = document.getElementById("email");
 const passwordField = document.getElementById("password");
 const regBtn = document.getElementById("regBtn");
 
-regBtn.addEventListener("click", (e) => {
-  const login = loginField.value;
-  const email = emailField.value;
-  const password = passwordField.value;
+regBtn.addEventListener("click", e => {
   e.preventDefault();
-  fetch("../api/users.php", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ login: login }),
-    credentials: "include",
-  })
-    .then((res) => res.json())
-    .then((result) => {
-      if (result.success) {
-        fetch("../api/users.php", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            login: login,
-            email: email,
-            password: password,
-          }),
-          credentials: "include",
-        });
-      } else {
-        console.error("Ошибка регистрации:", result.message);
-      }
-    })
-    .catch((err) => console.error(err));
 });
