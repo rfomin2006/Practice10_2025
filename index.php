54601cd5537815642f27565ea0c92f6eac739f4e
rfomin2006 <rfomin2805@gmail.com>
2025-10-16 14:53:36 +0700

    Used check_auth() in index.php. Documented check_auth function

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once './util/session_helper.php';
 session_start();
 
-header('Location: ' . (isset($_SESSION['uid']) ? './pages/home.php' : './pages/register.php'));
+header('Location: ' . (check_auth() ? './pages/home.php' : './pages/register.php'));
--- util/session_helper.php
+++ util/session_helper.php
@@ -1,4 +1,16 @@
-<?php function check_auth(): bool
+<?php
+
+/**
+ * Checks if a user is authenticated.
+ *
+ * This function determines whether a user is currently logged in by
+ * checking if the 'uid' key exists in the $_SESSION superglobal and
+ * has a truthy value.
+ *
+ * @return bool Returns `true` if the user is logged in (i.e., $_SESSION['uid'] exists and is truthy),
+ *              or `false` if the user is not logged in.
+ */
+function check_auth(): bool
 {
     return !!($_SESSION['uid'] ?? false);
 }
